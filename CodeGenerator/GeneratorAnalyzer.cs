ed6f6c4cd7f7999f121f9957f9c2cca4f1287bf0
VladD <VladD@TIGGER>
2025-02-23 19:31:29 +0100

    Moved Location info to a separate file

--- CodeGenerator/GeneratorAnalyzer.cs
+++ CodeGenerator/GeneratorAnalyzer.cs
@@ -99,13 +99,3 @@ public class GeneratorAnalyzer : IIncrementalGenerator
 }
 
 public record Model(string? Namespace, string ClassName, string PropertyName, int OutputType, LocationInfo? Location);
-
-// https://andrewlock.net/creating-a-source-generator-part-9-avoiding-performance-pitfalls-in-incremental-generators/
-public record LocationInfo(string FilePath, TextSpan TextSpan, LinePositionSpan LineSpan)
-{
-    public Location ToLocation() => Location.Create(FilePath, TextSpan, LineSpan);
-    public static LocationInfo? CreateFrom(SyntaxNode node) => CreateFrom(node.GetLocation());
-    public static LocationInfo? CreateFrom(Location location) => location.SourceTree is null ?
-            null :
-            new LocationInfo(location.SourceTree.FilePath, location.SourceSpan, location.GetLineSpan().Span);
-}
---
+++ CodeGenerator/LocationInfo.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+
+namespace CodeGenerator;
+
+// https://andrewlock.net/creating-a-source-generator-part-9-avoiding-performance-pitfalls-in-incremental-generators/
+public record LocationInfo(string FilePath, TextSpan TextSpan, LinePositionSpan LineSpan)
+{
+    public Location ToLocation() => Location.Create(FilePath, TextSpan, LineSpan);
+    public static LocationInfo? CreateFrom(SyntaxNode node) => CreateFrom(node.GetLocation());
+    public static LocationInfo? CreateFrom(Location location) => location.SourceTree is null ?
+            null :
+            new LocationInfo(location.SourceTree.FilePath, location.SourceSpan, location.GetLineSpan().Span);
+}
